f141e8158218ad391e1e2eae791318c2fe6484ef
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-08 16:17:05 +0530

    code prisma'

---
+++ prisma/migrations/20251108160427_add_city_column/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+-- This migration adds the city column. Since it may already exist in some databases,
+-- we use IF NOT EXISTS pattern for safety.
+DO \$\$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'User' AND column_name = 'city'
+    ) THEN
+        ALTER TABLE "User" ADD COLUMN "city" TEXT;
+    END IF;
+END \$\$;
