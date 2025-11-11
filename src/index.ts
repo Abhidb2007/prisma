649444f34e76c2be2995e6e7965962bd087e9065
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-11 23:21:52 +0530

    code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
+async function createUser() {
+  try {
+    const user = await client.user.create({
+      data: {
+        name: "abhu",
+        password: "abhu123",
+        age: 21,
+        city: "delhi",
+      },
+    });
+    console.log("User created:", user);
+  } catch (err) {
+    console.error("Error creating user:", err);
+  } finally {
+    await client.$disconnect();
+  }
+}
+
+createUser();
