b143babb82c268081d859024f8bf29d13342b8b8
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-12 00:27:03 +0530

    code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
+import express from "express";
 import { PrismaClient } from "@prisma/client";
+const app=express();
 const client = new PrismaClient();
 
+app.get("/users",async(req,res)=>{
+  const users=await client.users.findMany();
+  res.json(users)({
+     users
+  })
+})
+app.get("/todos/:id",async(req,res)=>{
+  const{id}=req.params.id as unknown as number;
+  const users=await client.users.findfirst({
+    where:{
+      id:id
+    }
+    select:{
+      todos:true
+    }
+  });
+  res.json(users)({
+     users
+  })  
+})
+app.listen(3000,()=>{
+  console.log("Server is running on port 3000");
+});
+
+  
+ 
 async function createUser() {
   try {
     const user = await client.user.create({
